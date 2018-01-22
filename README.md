cde70d15f1dff064de4e27236b478786757996d0
Rodrigo Garcia <rodrigo@rgnu.com.ar>
2018-01-22 10:57:44 -0300

    Initial Im port

---
+++ README.md
@@ -0,0 +1,13 @@
+## Serverless Test
+
+```
+$ npm i
+```
+
+
+## Deploy Hello World
+
+```
+$ cd helloworld && npm run sls -- deploy -s staging
+```
+
---
+++ helloworld/handler.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const bluebird = require('bluebird');
+
+module.exports.helloWorld = (event, context, callback) => {
+
+  console.time('event');
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+      env: process.env,
+      context: context
+    }),
+  };
+
+  callback(null, response);
+
+  console.log(event.httpMethod, event.path, response.statusCode, console.timeEnd('event'));
+
+};
